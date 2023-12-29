fea81a66dc18b10061f6605ffe19a38866fdb5c6
super-snus <120594283+super-snus@users.noreply.github.com>
2023-12-29 14:37:25 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,23 @@
+-- Инициализация окружения OpenComputers
+local component = require("component")
+local computer = require("computer")
 local shell = require("shell")
-shell.execute("/путь_к_вашей_MyOS.lua")
+
+-- Проверка наличия компонентов
+if not component.isAvailable("gpu") or not component.isAvailable("screen") then
+    error("Графический компонент не доступен")
+end
+
+-- Инициализация графической части, если она доступна
+local gpu = component.gpu
+local w, h = gpu.getResolution()
+gpu.setResolution(w, h)
+
+-- Очистка экрана
+gpu.fill(1, 1, w, h, " ")
+
+-- Пример использования print()
+print("Операционная система MyOS запущена")
+
+-- Пример команды
+shell.execute("ls")
